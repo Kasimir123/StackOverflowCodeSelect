4710d97b5ecdaa9670dfe9803f9166f4026db7a3
Kasimir Schulz <kasimirkhschulz@gmail.com>
2020-04-22 19:28:37 -0400

    Added an option to select how long a code block needs to be for a button to be added. also added sample icon

--- content.js
+++ content.js
@@ -4,19 +4,25 @@ var codes = document.getElementsByTagName("code");
 // Text for the button
 var buttonText = "Copy code";
 
-// Loops through all code objects on page and adds a button
-for (i = 0; i < codes.length; i++) {
-    // Creates button
-    var button = document.createElement("button");
-    // adds the text to the button
-    button.innerHTML = buttonText;
-    // Adds the button to the code block
-    codes[i].appendChild(button);
-    // Adds the event listener to the button
-    button.addEventListener('click', function() {
-        saveToClip(this.parentElement);
-    });
-}
+// Gets the stored values
+chrome.storage.sync.get(["minVal"], function(items) {
+    // Loops through all code objects on page and adds a button
+    for (i = 0; i < codes.length; i++) {
+        // Creates button
+        var button = document.createElement("button");
+        // adds the text to the button
+        button.innerHTML = buttonText;
+        // Checks if the value is a number and if the content is longer than the min length
+        if (!isNaN(items["minVal"]) && codes[i].textContent.length >= parseInt(items["minVal"])) {
+            // Adds the button to the code block
+            codes[i].appendChild(button);
+        }
+        // Adds the event listener to the button
+        button.addEventListener('click', function() {
+            saveToClip(this.parentElement);
+        });
+    }   
+});
 
 /*
  Takes in the code object, grabs all text - the length of the button text and
